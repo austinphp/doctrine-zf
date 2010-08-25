a5a80e7d2a1cc7dde4710f1cd7886050ac70a393
Josh Butts <josh@joshbutts.com>
2010-08-25 12:56:39 -0500

    updates

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,6 +10,18 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
+    	   $q = Doctrine_Query::create();
+    	   $q->from('Offer o')
+    	     ->where('o.id = ?', 1);
+    	     
+  
+    	     
+    	   /** 
+    	    * @var $offer Offer
+    	    */  
+           $offer = $q->fetchOne();
+           
+           Zend_Debug::dump($offer->Company->name);
     }
 
 
